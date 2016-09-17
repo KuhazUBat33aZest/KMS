7df2504c41c59a33b71b765ec4e59be78627cdb5
Nick <KuhazUBat33aZest@outlook.com>
2016-09-16 21:39:43 -0400

    Base Files
    Landing Page -> load angular, app.js, controller.js
    App.js -> Create the module
    Controller.js -> Create the controllers.

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
+<!doctype html>
+<html ng-app='KMS'>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-resource.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-route.js"></script>
+  <script src="js/app.js"></script>
+  <script src="js/controller.js"></script>
+  
+  <body>
+    <ng-view></ng-view>
 
-?>
+    <div>
+      <h2>Hello World!</h2>
+    </div>
+  </body>
+
+</html>
